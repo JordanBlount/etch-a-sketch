deef6bf3a0fdcbacc1e0c1db1756c24b585d244c
Jordan Blount <jordantblount@gmail.com>
2021-05-01 00:41:54 -0400

    Fixed CSS and added more game logic.
    I add styling for the board pieces and made the board larger.
    
    I added more logic for creating the "board pieces" in Javascript.
    
    I still have to create an algorithm for removing them in Javascript.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
 let changeBtn = document.querySelector('#change');
 let board = document.querySelector('#board');
-let numOfSqrs = 0;
+let currentNumSpaces = 0;
+let boardPieces = [];
 
+// Examine the logic here. We need the game to be pre-setup with 16 * 16.
+// We need to be able to ask for new number of squares, clear board if
+// needed, create new board. 
+// 
+
+// This button is for the player to select the size of the grid. 16 * 16
+// example.
 changeBtn.addEventListener('click', function() {
-    numOfSqrs = parseInt(window.prompt('How many squares do you want?', '15'), 10);
+    let numOfSqrs = parseInt(window.prompt('How many squares do you want?', '16'), 10);
     console.log(numOfSqrs);
     if(numOfSqrs >= 100) {
         alert("Choose a number less than 100.");
         return;
     } else {
         numOfSqrs = numOfSqrs * numOfSqrs;
+        currentNumSpaces = numOfSqrs;
         startGame(numOfSqrs);
     }
 });
@@ -18,25 +27,31 @@ function startGame(sqrs) {
     createBoard(sqrs);
 }
 
+function clearBoard(sqrs) {
+    for (i = 0; i < sqrs; i++) {
+        board.removeChild(boardPieces[i]);
+        console.log("Created square " + i);
+    }
+}
+
 function createBoard(spaces) {
-    let boardPieces = [];
+    boardPieces = [];
     for (i = 0; i < spaces; i++) {
-        console.log("Created div " + i);
         boardPieces.push(createDiv());
         board.appendChild(boardPieces[i]);
+        console.log("Created square " + i);
     }
-    document.querySelectorAll(".board-piece").foreach(piece => {
-        piece.addEventListener('mouseover', function(){
-            piece.classList.add("hovering");
-        });
-        piece.addEventListener('mouseoff', function(){
-            piece.classList.add("colored");
-        });
-    })
 }
 
 function createDiv() {
     let boardDiv = document.createElement('div');
-    boardDiv.classList.add("board-piece");
+    // boardDiv.classList.add("board-piece");
+    boardDiv.setAttribute('class', 'board-piece');
+    // boardDiv.addEventListener('mouseover', function(){
+    //     piece.classList.add("hovering");
+    // });
+    // boardDiv.addEventListener('mouseoff', function(){
+    //     piece.classList.add("colored");
+    // });
     return boardDiv;
 }
